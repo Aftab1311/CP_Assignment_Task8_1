c046f7ae9db4b1220db8d13473e1f5a88eaefa5a
Aftab Ahmed <88955526+Aftab1311@users.noreply.github.com>
2021-08-24 09:24:56 +0530

    Update task8_1.cpp

--- task8_1.cpp
+++ task8_1.cpp
@@ -1,3 +1,11 @@
+/*Given two strings A and B, the task is to print all the non-repeating words out of the two given sentences. input: A = “I have a blue pen”, B = “I got a red pen” 
+ Output: have blue got red 
+ Explanation: 
+ The words have, blue, got and red have not been repeated in either the same sentence or another  sentence. 
+*/
+
+
+
 #include <bits/stdc++.h>
 #include <string.h>
 using namespace std;
